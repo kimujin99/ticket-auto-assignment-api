ceb745758c274fd538f5495bd4766e8c8a17a6ea
kimujin99 <tomatoo3o97@gmail.com>
2025-08-06 13:20:50 +0900

    첫 커밋: 프로젝트 초기 세팅

---
+++ src/main/java/com/ticket/auto_assignment_api/AutoAssignmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.auto_assignment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoAssignmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoAssignmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticket/auto_assignment_api/AutoAssignmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.auto_assignment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoAssignmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
